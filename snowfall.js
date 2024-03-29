e9e4c5df0bd379489d7b2739ca1c20c023f6821e
Praveen Kumar <163954404+Praveen07d@users.noreply.github.com>
2024-03-29 22:48:17 +0530

    Update snowfall.js

--- snowfall.js
+++ snowfall.js
@@ -1,25 +1,10 @@
-/* media/style.css */
-
-body {
-    overflow: hidden; /* Prevents the snowflakes from overflowing the viewport */
-}
-
 .snowflake {
     position: fixed;
-    top: -10px; /* Start position */
-    z-index: 1000; /* Ensures the snowflakes stay on top of other content */
     width: 10px;
     height: 10px;
-    background: white;
+    background: rgba(255, 255, 255, 0.8);
     border-radius: 50%;
-    animation: snowfall linear infinite;
-}
-
-@keyframes snowfall {
-    0% {
-        transform: translateY(-10px) rotate(0deg);
-    }
-    100% {
-        transform: translateY(100vh) rotate(360deg);
-    }
+    filter: blur(2px);
+    transition: filter 0.5s;
+    z-index: 9999;
 }
